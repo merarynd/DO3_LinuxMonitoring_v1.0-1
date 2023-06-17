12ada7cf45d078d5f54fc94073c0649b100c017e
Mera Ryndoon <merarynd@pr-i2.kzn.21-school.ru>
2023-06-18 01:55:03 +0300

    2

---
+++ src/02/main.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+if [ $# -ne 0 ]; then
+  echo "Параметры не требуются"
+else bash param.sh
+fi
+
+# HOSTNAME = ws2
+# TIMEZONE = Etc/UTC UTC +03
+# USER = merarynd
+# OS = Ubuntu20.04.6LTS
+# DATE = 13 июн 2023 16:04:33
+# UPTIME = 3 hours,
+# UPTIME_SEC = 11581.75 sec
+# IP = 10.0.2.15 10.20.0.10 
+# MASK = 255.255.255.0
+# GATEWAY = 10.0.2.2
+# RAM_TOTAL = 0.953 GB
+# RAM_USED = 0.353 GB
+# RAM_FREE = 0.136 GB
+# SPACE_ROOT = 8212.00 MB
+# SPACE_ROOT_USER = 4395.00 MB
+# SPACE_ROOT_FREE = 3379.00 MB
+# Желаете сохранить данные в файл? (Y/N): n
+
+# HOSTNAME = ws2
+# TIMEZONE = Etc/UTC MSK +0300
+# USER = merarynd
+# No LSB modules are available.
+# OS = Ubuntu
+# 20.04.6LTS
+# DATE = Сб 17 июн 2023 22:24:24 MSK
+# UPTIME = up 23 minutes
+# UPTIME_SEC = 1394.20
+# IP = 10.0.2.15 10.20.0.10 
+# MASK = 255.255.255.0
+# 255.255.255.192
+# 255.0.0.0
+# GATEWAY = default via 10.0.2.2 dev enp0s3 proto dhcp src 10.0.2.15 metric 100 
+# RAM_TOTAL =1000108 K total memory
+# RAM_USED = размер используемой памяти в Гб c точностью три знака после запятой
+# RAM_FREE = размер свободной памяти в Гб c точностью три знака после запятой
+# SPACE_ROOT = размер рутового раздела в Mб с точностью два знака после запятой в виде: 254.25 MB
+# SPACE_ROOT_USED = размер занятого пространства рутового раздела в Mб с точностью два знака после запятой
+# SPACE_ROOT_FREE = размер свободного пространства рутового раздела в Mб с точностью два знака после запятой
---
+++ src/02/param.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "HOSTNAME = $HOSTNAME"
+echo "TIMEZONE = $(cat /etc/timezone) $(date +"%Z %z")"
+echo "USER = $USER"
+echo "OS = $(lsb_release -a | awk '{print $3 $4}')"
+echo "DATE = $(date)"
+echo "UPTIME = $(uptime -p)"
+echo "UPTIME_SEC = $(cat /proc/uptime | awk '{print $1}')"
+echo "IP = $(hostname -I)"
+echo "MASK = $(ifconfig | awk '/netmask/{print $4}')"
+echo "GATEWAY = $(ip route | grep default)"
+echo "RAM_TOTAL =$(free --bytes | awk '/Mem/{printf "%.3f", $2 / 1000000000}')"
+echo "RAM_USED = $(free --bytes | awk '/Mem/{printf "%.3f", $3 / 1000000000}')"
+echo "RAM_FREE = $(free --bytes | awk '/Mem/{printf "%.3f", $4 / 1000000000}')"
+echo "SPACE_ROOT = $(df -h /home | awk '/\/$/{printf "%.2f", $2 * 1024}')"
+echo "SPACE_ROOT_USED = $(df -h /home | awk '/\/$/{printf "%.2f", $3 * 1024}')"
+echo "SPACE_ROOT_FREE = $(df -h /home | awk '/\/$/{printf "%.2f", $4 * 1024}')"
